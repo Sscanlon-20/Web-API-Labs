b243e1fba3d5029e88d8413f2470e1791d406275
Your Name <you@example.com>
2023-04-26 10:48:22 +0100

    Topic 12 - Update Movies Data/Route

--- movies-api/index.js
+++ movies-api/index.js
@@ -5,6 +5,8 @@ import genresRouter from './api/genres';
 import './db'; 
 import './seedData';
 import usersRouter from './api/users';
+import passport from './authenticate';
+
 
 
 dotenv.config();
@@ -21,11 +23,14 @@ const app = express();
 
 const port = process.env.PORT;
 app.use(express.json());
-
-app.use('/api/movies', moviesRouter);
+app.use(passport.initialize());
+// Add passport.authenticate to middleware stack for protected routes​
+app.use('/api/movies', passport.authenticate('jwt', {session: false}), moviesRouter);
 app.use('/api/genres', genresRouter);   
 app.use('/api/users', usersRouter);
 app.use(errHandler);
+
+
 
 
 app.listen(port, () => {
