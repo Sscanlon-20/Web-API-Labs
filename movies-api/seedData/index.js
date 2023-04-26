c45a8ff228f234a7a03422c85c4766ddd8249394
Your Name <you@example.com>
2023-04-26 10:17:27 +0100

    Topic 12 - Salt and Hash

--- movies-api/seedData/index.js
+++ movies-api/seedData/index.js
@@ -3,7 +3,7 @@ import genreModel from '../api/genres/genreModel';
 import users from './users';
 import genres from './genres';
 import dotenv from 'dotenv';
-
+ 
 dotenv.config();
 
 // deletes all user documents in collection and inserts test data
@@ -11,13 +11,13 @@ async function loadUsers() {
   console.log('load user Data');
   try {
     await userModel.deleteMany();
-    await userModel.collection.insertMany(users);
+    await users.forEach(user => userModel.create(user));
     console.info(`${users.length} users were successfully stored.`);
   } catch (err) {
     console.error(`failed to Load user Data: ${err}`);
   }
 }
- 
+
 async function loadGenres() {
   console.log('load genre Data');
   try {
